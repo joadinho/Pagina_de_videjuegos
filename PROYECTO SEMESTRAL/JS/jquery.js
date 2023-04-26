bbe28490eb99b6c8fdc78f00f3b62f6181ec8c05
joadinho <joa.lisboa@duocuc.cl>
2023-04-26 11:58:13 -0400

    avance jquery

--- PROYECTO SEMESTRAL/JS/jquery.js
+++ PROYECTO SEMESTRAL/JS/jquery.js
@@ -6,5 +6,15 @@ $(document).ready(function(){
         var clave = $("#password").val();
         var con_clave = $("#con_password").val();
         var correo = $("#email").val();
+        var contacto = $("#telefono").val();
+        var edad = $("#edad").val();
+
+        let msjMostrar = "";
+        let enviar = false;
+
+        if(nombre.trim().lenght < 4 || nombre.trim().lenght > 10){
+            msjMostrar = msjMostrar + "El nombre debe tener entre 4 y 10 caracteres";
+            enviar = true;
+        }
     });
 });
