205275de1d447d593d787e756d220173d4a1fe85
joadinho <joa.lisboa@duocuc.cl>
2023-04-26 16:04:41 -0400

    jquery registrarse validar

--- PROYECTO SEMESTRAL/JS/jquery.js
+++ PROYECTO SEMESTRAL/JS/jquery.js
@@ -12,9 +12,69 @@ $(document).ready(function(){
         let msjMostrar = "";
         let enviar = false;
 
-        if(nombre.trim().lenght < 4 || nombre.trim().lenght > 10){
-            msjMostrar = msjMostrar + "El nombre debe tener entre 4 y 10 caracteres";
+        //validar nombre
+        if(nombre.trim().length < 4 || nombre.trim().length > 10){
+            msjMostrar = msjMostrar + "<br>El nombre debe tener entre 4 y 10 caracteres";
             enviar = true;
         }
+
+        var letra = nombre.trim().charAt(0);
+        if(!esMayuscula(letra)){
+            msjMostrar += "<br>El nombre debe comenzar con mayúscula";
+            enviar = true;
+        }
+
+        if(nombre == "1","2","3","4","5","6","7","8","9","0"){
+            msjMostrar += "<br>El nombre no puede contener numeros";
+            enviar = true;
+        }
+        if(nombre.trim() == ""){
+            msjMostrar += "<br>El nombre no puede estar vacio";
+            enviar = true;
+        }
+
+        //Validar Apellido
+        if(apellido.trim().length < 4 || nombre.trim().length > 10){
+            msjMostrar = msjMostrar + "<br>El apellido debe tener entre 4 y 10 caracteres";
+            enviar = true;
+        }
+
+        var letra = apellido.trim().charAt(0);
+        if(!esMayuscula(letra)){
+            msjMostrar += "<br>El apellido debe comenzar con mayúscula";
+            enviar = true;
+        }
+
+        if(apellido == "1","2","3","4","5","6","7","8","9","0"){
+            msjMostrar += "<br>El apellido no puede contener numeros";
+            enviar = true;
+        }
+
+        if(nombre.trim() == ""){
+            msjMostrar += "<br>El apellido no puede estar vacio";
+            enviar = true;
+        }
+
+
+        
+
+        if(enviar){
+            $("#warnings").html(msjMostrar);
+        }
+        else{
+            $("#warnings").html("Enviado");
+        }
     });
+    function esMayuscula(letra){
+        console.log("Estoy aqui");
+        console.log(letra);
+        console.log(letra.toUpperCase());
+
+        if(letra == letra.toUpperCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 });
