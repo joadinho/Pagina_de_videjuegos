22ed6c776b166e4e3403d561bca100a6f63db3ce
joadinho <joa.lisboa@duocuc.cl>
2023-04-29 00:33:37 -0400

    validaciones casi terminadas

--- PROYECTO SEMESTRAL/JS/jquery.js
+++ PROYECTO SEMESTRAL/JS/jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#formRegistro").submit(function(e){
+    $("#formlogin").submit(function(e){
         e.preventDefault();
         var nombre = $("#nombre").val();
         var apellido = $("#apellido").val();
@@ -8,71 +8,91 @@ $(document).ready(function(){
         var correo = $("#email").val();
         var contacto = $("#telefono").val();
         var edad = $("#edad").val();
+        var f_nac = $("#f_nac").val();
+
+
+
 
         let msjMostrar = "";
         let enviar = false;
 
         //validar nombre
         if(nombre.trim().length < 4 || nombre.trim().length > 10){
-            msjMostrar = msjMostrar + "El nombre debe tener entre 4 y 10 caracteres";
+            msjMostrar = msjMostrar + "-El nombre debe tener entre 4 y 10 caracteres";
             enviar = true;
         }
 
         var letra = nombre.trim().charAt(0);
         if(!esMayuscula(letra)){
-            msjMostrar += "<br>El nombre debe comenzar con mayúscula";
+            msjMostrar += "<br>-El nombre debe comenzar con mayúscula";
             enviar = true;
         }
     
         
         if(nombre.trim() == ""){
-            msjMostrar += "<br>El nombre no puede estar vacio";
+            msjMostrar += "<br>-El nombre no puede estar vacio";
             enviar = true;
         }
 
         //Validar Apellido
         if(apellido.trim().length < 4 || apellido.trim().length > 10){
-            msjMostrar = msjMostrar + "<br>El apellido debe tener entre 4 y 10 caracteres";
+            msjMostrar = msjMostrar + "<br>-El apellido debe tener entre 4 y 10 caracteres";
             enviar = true;
         }
 
         var letra = apellido.trim().charAt(0);
         if(!esMayuscula(letra)){
-            msjMostrar += "<br>El apellido debe comenzar con mayúscula";
+            msjMostrar += "<br>-El apellido debe comenzar con mayúscula";
             enviar = true;
         }
 
 
         if(apellido.trim() == ""){
-            msjMostrar += "<br>El apellido no puede estar vacio";
+            msjMostrar += "<br>-El apellido no puede estar vacio";
             enviar = true;
         }
 
         //validar password
-
-        
         if(clave.trim().length < 8 || clave.trim().length > 12){
-            msjMostrar = msjMostrar + "<br>La clave debe tener entre 8 y 12 caracteres";
+            msjMostrar = msjMostrar + "<br>-La clave debe tener entre 8 y 12 caracteres";
             enviar = true;
         }
-        if(clave.trim() == ""){
-            msjMostrar += "<br> La clave no puede estar vacia";
-            enviar = true;
-        }
-        
 
-        
+        if(clave.trim()  == ""){
+            msjMostrar += "<br>-La clave no puede estar vacia";
+            enviar = true;
+        }
+
+        if (!clave.match(/([A-Z])/)){
+           msjMostrar += "<br>-Falta una letra mayuscula";
+            enviar = true;
+        }
+
+        if (!clave.match(/([a-z])/)){
+            msjMostrar += "<br>-Falta una letra minuscula";
+             enviar = true;
+         }
+
+         if (!clave.match(/([0-9])/)){
+            msjMostrar += "<br>-Debe contener al menos un numero";
+            enviar = true;
+         }
+
+        if (!clave.match(/([!,%,&,@,#,$,^,,?,_,~])/)){
+            msjMostrar += "<br>-Debe contener un caracter especial";
+            enviar = true;
+         }
 
 
         //validar correo
 
         if((correo).trim().indexOf('@', 0) == -1 || (correo).trim().indexOf('.', 0) == -1) {
-            msjMostrar += "<br>El correo electrónico introducido es invalido. Debe contener un @";
+            msjMostrar += "<br>-El correo electrónico introducido es invalido. Debe contener un @";
             enviar = true;
         }
 
         if(correo.trim() == ""){
-            msjMostrar += "<br>El correo no puede estar vacio";
+            msjMostrar += "<br>-El correo no puede estar vacio";
             enviar = true;
         }
 
@@ -80,13 +100,69 @@ $(document).ready(function(){
 
 
         if(contacto.trim() == ""){
-            msjMostrar += "<br>El telefono no puede estar vacio";
+            msjMostrar += "<br>-El telefono no puede estar vacio";
             enviar = true;
         }
         if(contacto.trim().length < 9 || contacto.trim().length > 9){
-            msjMostrar += "<br> telefono invalido. Deben ser 9 numeros "
+            msjMostrar += "<br>-Telefono invalido. Deben ser 9 numeros "
             enviar = true;
         }
+
+        //validar edad
+        
+        if(edad < 15){
+            msjMostrar += "<br>-Para registrarse debe tener una edad minima de 15 años";
+            enviar = true;
+        }
+
+        //validar fecha nacimiento
+
+        function isValidDate(dateString){
+        // revisar el patrón
+            if(!/^\d{4}-\d{1,2}-\d{1,2}$/.test(dateString))
+            return false;
+
+        // convertir los numeros a enteros
+            var parts = dateString.split("/");
+            var day = parseInt(parts[2], 10);
+            var month = parseInt(parts[1], 10);
+            var year = parseInt(parts[0], 10);
+
+        // Revisar los rangos de año y mes
+            if( (year < 2008)  (year > 2023)  (month == 0)  (month > 12) ){
+
+            
+            return false;
+            }
+            var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+        // Ajustar para los años bisiestos
+            if(year % 400 == 0  (year % 100 != 0 && year % 4 == 0))
+                monthLength[1] = 29;
+                
+            if(year < 2008 ){
+            msjMostrar+="<br> fecha de nacimiento invalida ";
+            enviar = true;
+
+        }
+
+        // Revisar el rango del dia
+        return day > 0 && day <= monthLength[month - 1];
+        
+        
+        
+        };
+
+        
+        
+
+        $("#Enviar").click(function() {
+            alert( isValidDate($("#f_nac").val()) );
+
+            
+
+            });
+
         
 
         if(enviar){
@@ -95,6 +171,8 @@ $(document).ready(function(){
         else{
             $("#mensaje").html("Enviado");
         }
+
+        
 
     });
     function esMayuscula(letra){
